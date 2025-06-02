70e2aee09a7a49372402a862d2cb60c1b45b53fe
Gustavo Coronel <gcoronelc@gmail.com>
2025-06-02 16:10:49 -0500

    Semana 11

---
+++ Semana11/SolucionesEduca/src/main/java/pe/edu/uni/solucioneseduca/prueba/PruebaMatricula06TipoMatricula.java
@@ -0,0 +1,27 @@
+package pe.edu.uni.solucioneseduca.prueba;
+
+import pe.edu.uni.solucioneseduca.dto.MatriculadoDto;
+import pe.edu.uni.solucioneseduca.service.ProcesosSeervice;
+
+public class PruebaMatricula06TipoMatricula {
+
+	public static void main(String[] args) {
+		try {
+			// Datos
+			MatriculadoDto bean = new MatriculadoDto();
+			bean.setIdEmpleado(3); // Pasa
+			bean.setIdAlumno(6); //  Pasa
+			bean.setIdCurso(1); //  pasa
+			bean.setTipoMatricula("LIBRE"); // NO pasa
+			// Proceso
+			ProcesosSeervice procesosSeervice;
+			procesosSeervice = new ProcesosSeervice();
+			procesosSeervice.procMatricula(bean);
+			// Reporte
+			System.out.println("Matricula ok.");
+		} catch (Exception e) {
+			System.err.println("ERROR: " + e.getMessage());
+		}
+	}
+	
+}
