f84f2c8dc1d91483969182986da6c410de439cfa
Gustavo Coronel <gcoronelc@gmail.com>
2025-06-09 15:57:23 -0500

    Semana 12

---
+++ Semana12/SolucionesEduca/src/main/java/pe/edu/uni/solucioneseduca/prueba/PruebaMatricula02Alumno.java
@@ -0,0 +1,25 @@
+package pe.edu.uni.solucioneseduca.prueba;
+
+import pe.edu.uni.solucioneseduca.dto.MatriculadoDto;
+import pe.edu.uni.solucioneseduca.service.ProcesosSeervice;
+
+public class PruebaMatricula02Alumno {
+
+	public static void main(String[] args) {
+		try {
+			// Datos
+			MatriculadoDto bean = new MatriculadoDto();
+			bean.setIdEmpleado(3); // Pasa
+			bean.setIdAlumno(333); // No pasa
+			// Proceso
+			ProcesosSeervice procesosSeervice;
+			procesosSeervice = new ProcesosSeervice();
+			procesosSeervice.procMatricula(bean);
+			// Reporte
+			System.out.println("Matricula ok.");
+		} catch (Exception e) {
+			System.err.println("ERROR: " + e.getMessage());
+		}
+	}
+	
+}
---
+++ Semana12/SolucionesEduca/src/main/java/pe/edu/uni/solucioneseduca/prueba/PruebaMatricula10MatriculaOk.java
@@ -0,0 +1,29 @@
+package pe.edu.uni.solucioneseduca.prueba;
+
+import pe.edu.uni.solucioneseduca.dto.MatriculadoDto;
+import pe.edu.uni.solucioneseduca.service.ProcesosSeervice;
+
+public class PruebaMatricula10MatriculaOk {
+
+	public static void main(String[] args) {
+		try {
+			// Datos
+			MatriculadoDto bean = new MatriculadoDto();
+			bean.setIdEmpleado(3); // Pasa
+			bean.setIdAlumno(6); //  Pasa
+			bean.setIdCurso(1); //  pasa
+			bean.setTipoMatricula("REGULAR"); // pasa
+			bean.setPrecio(1000.0);
+			bean.setCuotas(2);
+			// Proceso
+			ProcesosSeervice procesosSeervice;
+			procesosSeervice = new ProcesosSeervice();
+			procesosSeervice.procMatricula(bean);
+			// Reporte
+			System.out.println("Matricula ok.");
+		} catch (Exception e) {
+			System.err.println("ERROR: " + e.getMessage());
+		}
+	}
+	
+}
